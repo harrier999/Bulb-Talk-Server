794cde9fca9a3db6b96c844e6a23aadfbc69d9d8
harrier999 <harrier999@naver.com>
2023-08-06 16:41:19 +0900

    [FEAT] Singletone pattern to redis connection

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,26 +5,29 @@ import (
 	"os"
 	"context"
 	"log"
+	"sync"
 )
 
 var ctx = context.Background()
+var redisDB *redis.Client
+var once sync.Once
 
 func GetRedisClient() *redis.Client {
-	
-	redis_addr := os.Getenv("REDIS_ADDR")
-	redis_password := os.Getenv("REDIS_PASS")
-	log.Println("redis addr is ", redis_addr)
-	log.Println("redis password is ", redis_password)
-
-	redisDB := redis.NewClient(&redis.Options{
-		Addr:     redis_addr,
-		Password: redis_password,
-		DB:       0,
+	once.Do(func() {
+		log.Println("Connecting to redis...")
+		redis_addr := os.Getenv("REDIS_ADDR")
+		redis_password := os.Getenv("REDIS_PASS")
+		redisDB = redis.NewClient(&redis.Options{
+			Addr:     redis_addr,
+			Password: redis_password,
+			DB:       0,
+		})
+		_, err := redisDB.Ping(ctx).Result()
+		if err != nil {
+			log.Fatalln("Error connecting to redis. Error: ", err.Error())
+			panic(err.Error())
+		}
 	})
-	_, err := redisDB.Ping(ctx).Result()
-	if err != nil {
-		panic(err.Error())
-	}
 
 	return redisDB
 }
