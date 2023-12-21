88c7d6acea9a3ece287f6aa29c8edf1d1a15ab03
harrier999 <harrier999@naver.com>
2023-12-21 16:21:37 +0900

    [ADD] ORM Models

---
+++ internal/models/orm/rdb_models.go
@@ -0,0 +1,41 @@
+package orm
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	UserID       uuid.UUID      `gorm:"primaryKey; type:uuid; not null; default:gen_random_uuid()"`
+	UserName     string         `gorm:"type:varchar(40);not null"`
+	PasswordHash string         `gorm:"type:varchar(255);not null"`
+	ProfileImage sql.NullString `gorm:"type:varchar(255)"`
+	PhoneNumber  string         `gorm:"type:varchar(24);not null"`
+	CountryCode  string         `gorm:"type:varchar(8);not null"`
+	Email        sql.NullString `gorm:"type:varchar(64)"`
+	OauthID      sql.NullString `gorm:"type:varchar(255)"`
+	CreatedAt    sql.NullTime   `gorm:"type:timestamp;not null;default:now()"`
+}
+
+type DeviceList struct {
+	DeviceID uuid.UUID `gorm:"primaryKey; type:uuid; not null; default:gen_random_uuid()"`
+	UserID   uuid.UUID `gorm:"type:uuid;not null"`
+	Alarm    bool      `gorm:"type:boolean;not null;default:true"`
+}
+
+type FriendList struct {
+	UserID    uuid.UUID `gorm:"primaryKey; type:uuid; not null"`
+	FriendID  uuid.UUID `gorm:"type:uuid;not null"`
+	IsBlocked bool      `gorm:"type:boolean;not null;default:false"`
+}
+
+type RoomList struct {
+	RoomID   uuid.UUID `gorm:"primaryKey; type:uuid; not null; default:gen_random_uuid()"`
+	RoomName string    `gorm:"type:varchar(40);not null"`
+}
+
+type RoomUserList struct {
+	ID     uint      `gorm:"primarykey"`
+	RoomID uuid.UUID `gorm:"type:uuid; not null; default:gen_random_uuid()"`
+	UserID uuid.UUID `gorm:"type:uuid;not null"`
+}
