d24a0e06475ca14b93f718de6c7ab79eb8939c61
harrier999 <harrier999@naver.com>
2024-01-19 15:59:10 +0900

    [FEAT] Set JWT expiration time and test

--- pkg/create_token.go
+++ pkg/create_token.go
@@ -1,12 +1,17 @@
 package authenticator
 
-import "github.com/golang-jwt/jwt"
+import (
+	"time"
 
-func CreateToken(user_id string) (string, error) {
+	"github.com/golang-jwt/jwt"
+)
+
+func CreateToken(user_id string, durationHours int64) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 	token.Header["alg"] = "HS256"
 	claims[string(contextKeyUserID)] = user_id
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(durationHours)).Unix()
 	tokenString, err := token.SignedString(hmacSecret)
 	if err != nil {
 		return "", err
