d20c6211b51108dda38e701c8d84bbdf88621d1f
harrier999 <harrier999@naver.com>
2024-01-29 17:16:22 +0900

    [FIX] return uuid.Nil rather than uuid.UUID{}

--- pkg/authenticator/get_user_id.go
+++ pkg/authenticator/get_user_id.go
@@ -14,15 +14,15 @@ func GetUserID(r *http.Request) (uuid.UUID, error) {
 	userID, ok := r.Context().Value(ContextKeyUserID).(string)
 	if !ok {
 		logger.Error("Could not get user_id from context")
-		return uuid.UUID{}, errors.New("could not get user_id from context")
+		return uuid.Nil, errors.New("could not get user_id from context")
 	}
 	if userID == "" {
 		logger.Error("User_id is empty")
-		return uuid.UUID{}, errors.New("user_id is empty")
+		return uuid.Nil, errors.New("user_id is empty")
 	}
 	if !IsValidUUID(userID) {
 		logger.Error("User_id is invalid UUID", "user_id", userID)
-		return uuid.UUID{}, errors.New("user_id is invalid UUID")
+		return uuid.Nil, errors.New("user_id is invalid UUID")
 	}
 	return uuid.Parse(userID)
 }
