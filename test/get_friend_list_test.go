5da86d3d8fbb348def874479b3c52ae6f0b40ab5
harrier999 <harrier999@naver.com>
2024-01-29 15:54:22 +0900

    [REF] Refactor test code

--- test/get_friend_list_test.go
+++ test/get_friend_list_test.go
@@ -1,21 +1,16 @@
 package test
 
 import (
-	"bytes"
 	"encoding/json"
-
 	"net/http"
-	"net/http/httptest"
+
 	"testing"
 	"time"
 
 	"server/internal/handler/friends"
-
-	"server/pkg/authenticator"
+	"server/pkg/tutils"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/gorilla/mux"
 )
 
 var (
@@ -25,61 +20,41 @@ var (
 	_VALID_USER_ID_1 = "b5b06de3-5a19-4420-a6ae-cc84b4439a61"
 )
 
-func createRouter() *mux.Router {
-	r := mux.NewRouter()
-	r.Use(authenticator.JWTMiddleware)
-	r.HandleFunc("/friends", friends.GetFriendList).Methods("POST")
-	return r
-}
-
 func TestNormalCase(t *testing.T) {
-	r := createRouter()
-
+	r := tutils.CreateRouterWithMiddleware(friends.GetFriendList)
 	requestData := friends.FriendListRequest{
 		LastRequestTime: time.Now(),
 	}
 	reqBody, _ := json.Marshal(requestData)
 
-	req := httptest.NewRequest("POST", "/friends", bytes.NewBuffer(reqBody))
-	rr := httptest.NewRecorder()
-	token, _ := authenticator.CreateToken(_VALID_USER_ID_1, 24)
-	req.Header.Set("Authorization", token)
+	req, rr := tutils.CreateRequestAndResponse(reqBody)
+	req.Header.Set("Authorization", tutils.CreateTokenByString(_VALID_USER_ID_1))
 
 	r.ServeHTTP(rr, req)
 	if rr.Result().StatusCode != http.StatusOK {
 		t.Errorf("failed to get friend list")
 	}
-
 }
 
 func TestInvalidUUID(t *testing.T) {
-	r := createRouter()
+	r := tutils.CreateRouterWithMiddleware(friends.GetFriendList)
 
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("POST", "/friends", nil)
-	token, _ := authenticator.CreateToken(_WRONG_USER_ID_1, 24)
-	req.Header.Set("Authorization", token)
-
+	req, res := tutils.CreateRequestAndResponse(nil)
+	req.Header.Set("Authorization", tutils.CreateTokenByString(_WRONG_USER_ID_1))
 	r.ServeHTTP(res, req)
 	assert.Equal(t, http.StatusUnauthorized, res.Result().StatusCode)
 
-	res2 := httptest.NewRecorder()
-	req2 := httptest.NewRequest("POST", "/friends", nil)
-	token2, _ := authenticator.CreateToken(_WRONG_USER_ID_2, 24)
-
-	req2.Header.Set("Authorization", token2)
+	req2, res2 := tutils.CreateRequestAndResponse(nil)
+	req2.Header.Set("Authorization", tutils.CreateTokenByString(_WRONG_USER_ID_2))
 	r.ServeHTTP(res2, req2)
-
 	assert.Equal(t, http.StatusUnauthorized, res2.Result().StatusCode)
 }
 
 func TestEmptyFriend(t *testing.T) {
-	r := createRouter()
+	r := tutils.CreateRouterWithMiddleware(friends.GetFriendList)
 
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("POST", "/friends", nil)
-	token, _ := authenticator.CreateToken(_VALID_USER_ID_1, 24)
-	req.Header.Set("Authorization", token)
+	req, res := tutils.CreateRequestAndResponse(nil)
+	req.Header.Set("Authorization", tutils.CreateTokenByString(_VALID_USER_ID_1))
 
 	r.ServeHTTP(res, req)
 	t.Logf("res: %v", res.Body)
