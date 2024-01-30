9395629b68404a443d4383568527502723601b57
harrier999 <harrier999@naver.com>
2024-01-30 15:53:13 +0900

    Revert "[FEAT] Add GetRoomListHandler and test code"
    This reverts commit 9778de2b0c4a739f6067c6cf067d0797a6756812.

--- internal/handler/room/get_room_list.go
+++ internal/handler/room/get_room_list.go
@@ -1,40 +1,42 @@
 package room
 
 import (
-	"encoding/json"
+	"log"
 	"net/http"
 	"server/internal/db/postgres_db"
-	"server/pkg/authenticator"
-	"server/pkg/log"
-
+	"server/internal/models/orm"
 	"github.com/google/uuid"
 )
 
-type GetRoomListResponse struct {
-	RoomID   uuid.UUID `json:"room_id"`
-	RoomName string    `json:"room_name"`
-}
-
-func GetRoomListHandler(w http.ResponseWriter, r *http.Request) {
-	logger := log.NewColorLog()
-	user_id, err := authenticator.GetUserID(r)
-	if err != nil {
-		logger.Error(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
+func Handler(w http.ResponseWriter, r *http.Request) {
 	postgresClient := postgres_db.GetPostgresClient()
-	var roomList []GetRoomListResponse
-	postgresClient.Table("rooms").Select("rooms.room_id, rooms.room_name").
-		Joins("left join room_users on rooms.room_id = room_users.room_id").
-		Where("room_users.user_id = ?", user_id).Scan(&roomList)
 
-	err = json.NewEncoder(w).Encode(roomList)
+	user, err := GetUserId(r.Header)
 	if err != nil {
-		logger.Error(err.Error())
+		log.Println("Error getting user id. Error: ", err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+
+	roomUserList := []orm.RoomUserList{}
+	roomUser := orm.RoomUserList{}
+	roomUser.UserID = uuid.MustParse(user)
+
+	postgresClient.Model(&orm.RoomUserList{}).Where(&roomUser).Find(&roomUserList)
+
+	result := encoder(roomUserList)
 	w.WriteHeader(http.StatusOK)
+	w.Write(result)
+	
 }
+
+func encoder(roomUserList []orm.RoomUserList) []byte {
+	// TODO: implement encoder
+	return []byte{}
+}
+
+func GetUserId(header http.Header) (string, error) {
+	// TODO: implement jwt authorization
+	return header.Get("user_id"), nil
+}
+
