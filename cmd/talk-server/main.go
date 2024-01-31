cff648db60a4464ee7b3c868bb8a1d7130819535
harrier999 <harrier999@naver.com>
2024-01-31 10:50:03 +0900

    [FEAT] Enable Handlers

--- cmd/talk-server/main.go
+++ cmd/talk-server/main.go
@@ -2,21 +2,41 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"server/internal/handler/chatting"
+	"server/pkg/authenticator"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"server/internal/handler/room"
+	"server/internal/handler/user"
+	"server/internal/handler/friends"
+	"net/http"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
+		log.Println(err.Error())
 		log.Fatal("Error loading .env file")
 	}
-
-
 	r := mux.NewRouter()
-	r.HandleFunc("/chat", chatting.Handler).Methods("GET")
-	log.Fatal(http.ListenAndServe(":18000", r))
+
+
+
+	r.HandleFunc("/signup", user.SignUp).Methods("POST")
+	r.HandleFunc("/login", user.Login).Methods("POST")
+	r.HandleFunc("/authenticate", user.RequestAuthNumber).Methods("POST")
+	r.HandleFunc("/checkauth", user.CheckAuthNumber).Methods("POST")
+
+	authroizedRouter := r.PathPrefix("/auth").Subrouter()
+	authroizedRouter.Use(authenticator.JWTMiddleware)
+	authroizedRouter.HandleFunc("/getfriends", friends.GetFriendList).Methods("POST")
+	authroizedRouter.HandleFunc("/addfriends", friends.AddFriend).Methods("POST")
+	authroizedRouter.HandleFunc("/rooms", room.GetRoomListHandler).Methods("POST")
+	authroizedRouter.HandleFunc("/createrooms", room.CreateRoomHandler).Methods("POST")
+
+	port := ":18000"
+	
+	log.Println("Server is successfully running on port " + port)
+	log.Fatal(http.ListenAndServe(port, authroizedRouter))
+	
 }
